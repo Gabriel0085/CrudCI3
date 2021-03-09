6e2db05d5e892c1477bc52feb8434ed8245d4541
Thiago <thiago.silva@gruposn.com.br>
2021-03-09 13:37:54 -0300

    Inicializar crud
    Inicializar crud

---
+++ application/views/home.php
@@ -0,0 +1,301 @@
+<!-- Header -->
+<header>
+			<div class="container">
+				<div class="slider-container">
+					<div class="intro-text">
+						<div class="intro-lead-in">Aprenda com profissionais</div>
+						<div class="intro-heading">Code Burn treinamentos</div>
+						<a href="#about" class="page-scroll btn btn-xl">Saber mais</a>
+					</div>
+				</div>
+			</div>
+</header>
+<section id="about" class="light-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>Sobre</h2>
+							<p>
+                                A Code Burn é uma agência de treinamentos relacionados a tecnologias, como programação, redes e segurança.
+                            </p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /.container -->
+		</section>
+		
+		<section class="overlay-dark bg-img1 dark-bg short-section">
+			<div class="container text-center">
+				<div class="row">
+					<div class="col-md-3 mb-sm-30">
+						<div class="counter-item">
+							<h2 data-count="59">5</h2>
+							<h6>Professores</h6>
+						</div>
+					</div>
+					<div class="col-md-3 mb-sm-30">
+						<div class="counter-item">
+							<h2 data-count="1054">1054</h2>
+							<h6>Alunos</h6>
+						</div>
+					</div>
+					<div class="col-md-3 mb-sm-30">
+						<div class="counter-item">
+							<h2 data-count="34">7</h2>
+							<h6>Cursos</h6>
+						</div>
+					</div>
+					<div class="col-md-3 mb-sm-30">
+						<div class="counter-item">
+							<h2 data-count="154">853</h2>
+							<h6>Avaliações</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="portfolio" class="light-bg">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<div class="section-title">
+						<h2>Cursos</h2>
+						<p>
+                            Estes são os nosso cursos oferecidos:
+                        </p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<!-- start portfolio item -->
+				<div class="col-md-4">
+					<div class="ot-portfolio-item">
+						<figure class="effect-bubba">
+							<img src="images/demo/portfolio-1.jpg" alt="img02" class="img-responsive" />
+							<figcaption>
+								<h2>Dean & Letter</h2>
+								<p>Branding, Design</p>
+								<a href="#" data-toggle="modal" data-target="#Modal-1">View more</a>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<!-- end portfolio item -->
+				<!-- start portfolio item -->
+				<div class="col-md-4">
+					<div class="ot-portfolio-item">
+						<figure class="effect-bubba">
+							<img src="images/demo/portfolio-2.jpg" alt="img02" class="img-responsive" />
+							<figcaption>
+								<h2>Startup Framework</h2>
+								<p>Branding, Web Design</p>
+								<a href="#" data-toggle="modal" data-target="#Modal-2">View more</a>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<!-- end portfolio item -->
+				<!-- start portfolio item -->
+				<div class="col-md-4">
+					<div class="ot-portfolio-item">
+						<figure class="effect-bubba">
+							<img src="images/demo/portfolio-3.jpg" alt="img02" class="img-responsive" />
+							<figcaption>
+								<h2>Lamp & Velvet</h2>
+								<p>Branding, Web Design</p>
+								<a href="#" data-toggle="modal" data-target="#Modal-3">View more</a>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<!-- end portfolio item -->
+			</div>
+			<div class="row">
+				<!-- start portfolio item -->
+				<div class="col-md-4">
+					<div class="ot-portfolio-item">
+						<figure class="effect-bubba">
+							<img src="images/demo/portfolio-4.jpg" alt="img02" class="img-responsive" />
+							<figcaption>
+								<h2>Smart Name</h2>
+								<p>Branding, Design</p>
+								<a href="#" data-toggle="modal" data-target="#Modal-4">View more</a>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<!-- end portfolio item -->
+				<!-- start portfolio item -->
+				<div class="col-md-4">
+					<div class="ot-portfolio-item">
+						<figure class="effect-bubba">
+							<img src="images/demo/portfolio-5.jpg" alt="img02" class="img-responsive" />
+							<figcaption>
+								<h2>Fast People</h2>
+								<p>Branding, Web Design</p>
+								<a href="#" data-toggle="modal" data-target="#Modal-5">View more</a>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<!-- end portfolio item -->
+				<!-- start portfolio item -->
+				<div class="col-md-4">
+					<div class="ot-portfolio-item">
+						<figure class="effect-bubba">
+							<img src="images/demo/portfolio-2.jpg" alt="img02" class="img-responsive" />
+							<figcaption>
+								<h2>Kites & Stars</h2>
+								<p>Branding, Web Design</p>
+								<a href="#" data-toggle="modal" data-target="#Modal-2">View more</a>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<!-- end portfolio item -->
+			</div>
+			</div><!-- end container -->
+		</section>
+		<section id="team" class="light-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>Nossos professores</h2>
+							<p>
+                                A Code Burn conta com professores extremamente qualificados! 
+                            </p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<!-- team member item -->
+					<div class="col-md-3">
+						<div class="team-item">
+							<div class="team-image">
+								<img src="images/demo/author-2.jpg" class="img-responsive" alt="author">
+							</div>
+							<div class="team-text">
+								<h3>TOM BEKERS</h3>
+								<div class="team-location">Sydney, Australia</div>
+								<div class="team-position">– CEO & Web Design –</div>
+								<p>Mida sit una namet, cons uectetur adipiscing adon elit. Aliquam vitae barasa droma.</p>
+							</div>
+						</div>
+					</div>
+					<!-- end team member item -->
+					<!-- team member item -->
+					<div class="col-md-3">
+						<div class="team-item">
+							<div class="team-image">
+								<img src="images/demo/author-6.jpg" class="img-responsive" alt="author">
+							</div>
+							<div class="team-text">
+								<h3>LINA GOSATA</h3>
+								<div class="team-location">Los Angeles, California</div>
+								<div class="team-position">– Photography –</div>
+								<p>Worsa dona namet, cons uectetur dipiscing adon elit. Aliquam vitae fringilla unda mir.</p>
+							</div>
+						</div>
+					</div>
+					<!-- end team member item -->
+					<!-- team member item -->
+					<div class="col-md-3">
+						<div class="team-item">
+							<div class="team-image">
+								<img src="images/demo/author-3.jpg" class="img-responsive" alt="author">
+							</div>
+							<div class="team-text">
+								<h3>Larry Parker</h3>
+								<div class="team-location">Barcelona, Spain</div>
+								<div class="team-position">– Development –</div>
+								<p>Aradan bes namet, cons uectetur moiscing adon elit. Aliquam vitae fringilla unda augue.</p>
+							</div>
+						</div>
+					</div>
+					<!-- end team member item -->
+					<!-- team member item -->
+					<div class="col-md-3">
+						<div class="team-item">
+							<div class="team-image">
+								<img src="images/demo/author-4.jpg" class="img-responsive" alt="author">
+							</div>
+							<div class="team-text">
+								<h3>John BEKERS</h3>
+								<div class="team-location">Miami, Floria</div>
+								<div class="team-position">– Marketing –</div>
+								<p>Dolor sit don namet, cons uectetur beriscing adon elit. Aliquam vitae fringilla unda.</p>
+							</div>
+						</div>
+					</div>
+					<!-- end team member item -->
+				</div>
+			</div>
+		</section>
+		<section id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>Entre em contato</h2>
+                            <p>
+                                Caso tenha alguma dúvida, e nós pudermos ajudar, por favor entre em contato!<br>
+                                Entraremos em contato o mais rápido que conseguirmos.
+                            </p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+						<h3>Nosso escritório</h3>
+						<p>Rua Dr. Lourenço Menicucci Filho, Retiro 118, Lavras, Minas Gerais, 37203-525</p>
+						<p><i class="fa fa-phone"></i> (35)9.8876-2348</p>
+						<p><i class="fa fa-envelope"></i> codeburn@gmail.com</p>
+					</div>
+					<div class="col-md-3">
+						<h3>Horário de atendimento</h3>
+						<p><i class="fa fa-clock-o"></i> <span class="day">Semana:</span><span>08:00 ate 18:00</span></p>
+						<p><i class="fa fa-clock-o"></i> <span class="day">Sabado:</span><span>08:00 ate 12:00</span></p>
+						<p><i class="fa fa-clock-o"></i> <span class="day">Domingo:</span><span>Fechado</span></p>
+					</div>
+					<div class="col-md-6">
+						<form name="sentMessage" id="contactForm" novalidate="">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Seu nome *" id="name" required="" data-validation-required-message="Please enter your name.">
+										<p class="help-block text-danger"></p>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<input type="email" class="form-control" placeholder="Email *" id="email" required="" data-validation-required-message="Please enter your email address.">
+										<p class="help-block text-danger"></p>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="form-control" placeholder="Sua mensagem *" id="message" required="" data-validation-required-message="Please enter a message."></textarea>
+										<p class="help-block text-danger"></p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="row">
+								<div class="col-lg-12 text-center">
+									<div id="success"></div>
+									<button type="submit" class="btn">Enviar mensagem</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+		<p id="back-top">
+			<a href="#top"><i class="fa fa-angle-up"></i></a>
+		</p>
